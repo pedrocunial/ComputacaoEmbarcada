61d86a5fb21ccc0ac3ff208c03d2a55f3510e3be
Pedro Cunial <pedrocunial@gmail.com>
2017-02-08 17:41:09 -0200

    added tabela.c

---
+++ mutiraoC/tabela.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+typedef struct type_product
+{
+  char* name;
+  unsigned int qtd;
+  float price;
+  int* ptr_product;
+} product;
+
+
+void create_table()
+{
+
+}
+
+int main()
+{
+  return 0;
+}
