8f0ee9e6533efa7d7b2fcb31b280ce085d0d3bfc
Pedro Cunial <pedrocunial@gmail.com>
2017-02-09 14:20:19 -0200

    Quick commit - Thu Feb  9 14:20:18 BRST 2017

--- mutiraoC/tabela.c
+++ mutiraoC/tabela.c
@@ -1,20 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct type_product
 {
-  char* name;
-  unsigned int qtd;
-  float price;
-  int* ptr_product;
+    char name[20];
+    unsigned short qtd;
+    float price;
 } product;
 
 
-void create_table()
+product* create_table()
 {
+    unsigned short n_products;
+    product* products;
 
+    printf("Quantas comidas você comprou?\n");
+    scanf("%d", &n_products);
+    products = malloc(n_products * sizeof(product));
+
+    for (n_products; n_products--;) {
+        printf("Entre o nome do produto:\n");
+        scanf("%s", &products[n_products].name);
+        printf("Entre o preco unitario do produto:\n");
+        scanf("%f", &products[n_products].price);
+        printf("Entre a quantidade comprada:\n");
+        scanf("%d", &products[n_products].qtd);
+    }
+    return products;
 }
 
 int main()
 {
-  return 0;
+    product* products = create_table();
+    unsigned short n_products = sizeof(products) / sizeof(products[0]);
+
+    printf("| Nome do produto | Preço Unitário | Quantidade comprada |");
+    printf(" Total do Produto |\n");
+
+    for (n_products; n_products--;) {
+        printf("| %s | %f | %d | %f |",
+               products[n_products].name,
+               products[n_products].price,
+               products[n_products].qtd,
+               products[n_products].price * products[n_products].qtd);
+    }
+    return 0;
 }
