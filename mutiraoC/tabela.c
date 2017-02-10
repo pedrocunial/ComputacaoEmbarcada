ad78771ffb428b33c162cb519be384bc798bda28
Pedro Cunial <pedrocunial@gmail.com>
2017-02-10 08:53:25 -0200

    Quick commit - Fri Feb 10 08:53:25 BRST 2017

--- mutiraoC/tabela.c
+++ mutiraoC/tabela.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define PROD_NAME_SIZE 15
+
+const char *product_name = "Nome do Produto";
+const char *unit_price = "Preço Unitário";
+const char *bought_qtd = "Quantidade Comprada";
+const char *product_total = "Total do Produto";
 
 typedef struct type_product
 {
@@ -33,12 +41,25 @@ int main()
     products = malloc(n_products * sizeof(product));
     create_table(products, n_products);
 
-    printf("| Nome do produto | Preço Unitário | Quantidade comprada |");
-    printf(" Total do Produto |\n");
+    printf("| %s | %s | %s | %s |\n",
+           product_name,
+           unit_price,
+           bought_qtd,
+           product_total);
 
     for (n_products; n_products--;) {
-        printf("| %s | %f | %d | %f |",
-               products[n_products].name,
+        char prod_name_lenght = strlen(products[n_products].name);
+        prod_name_lenght = PROD_NAME_SIZE - prod_name_lenght;
+        if (prod_name_lenght < 0) {
+            prod_name_lenght = 0;
+            products[n_products].name[PROD_NAME_SIZE] = 0;
+        }
+
+        printf("| %s ", products[n_products].name);
+        for (prod_name_lenght; prod_name_lenght--;)
+            printf(" ");
+
+        printf("| %4.2f           | %4.2d                | %4.2f             |\n",
                products[n_products].price,
                products[n_products].qtd,
                products[n_products].price * products[n_products].qtd);
